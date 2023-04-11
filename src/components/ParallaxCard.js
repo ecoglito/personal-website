666889b45b10c198b9fb967abca9e7a6c9b35549
ecoglito <ecoglito@usc.edu>
2023-04-11 15:39:48 +0200

    fix a href

--- src/components/ParallaxCard.js
+++ src/components/ParallaxCard.js
@@ -84,11 +84,12 @@ export default function ParallaxCard( {list}  ) {
                 
                 <h3 className={styles.cardTitle}>Work</h3>
                
-
                 <div className = {styles.companies}>
                     <div className={styles.companyContainer}>
                         <div className={styles.company}>
+                          <a href = "https://www.alchemy.com">
                             <Image src="/alchemy.png" alt="Alchemy" className = {styles.companyImage} width={48} height={48} />
+                          </a>
                         </div>
                         <div className = {styles.companyText}>
                             <h4 className={styles.cardSubtitle}>Alchemy, June 2021 - Present</h4>
@@ -98,7 +99,9 @@ export default function ParallaxCard( {list}  ) {
 
                     <div className={styles.companyContainer}>
                         <div className={styles.company}>
-                            <Image src="/rstor.jpeg" alt="Rstor" className = {styles.companyImage} width={48} height={48} />
+                            <a href = "rstor.io">
+                              <Image src="/rstor.jpeg" alt="Rstor" className = {styles.companyImage} width={48} height={48} />
+                            </a>
                         </div>
                         <div className = {styles.companyText}>
                             <h4 className={styles.cardSubtitle}>RSTOR, June 2019 - August 2019</h4>
@@ -108,7 +111,9 @@ export default function ParallaxCard( {list}  ) {
 
                     <div className={styles.companyContainer}>
                         <div className={styles.company}>
-                            <Image src="/mindpump.png" alt="MindPump" className = {styles.companyImage} width={48} height={48} />
+                            <a href = "mindpumpmedia.com">
+                              <Image src="/mindpump.png" alt="MindPump" className = {styles.companyImage} width={48} height={48} />
+                            </a>
                         </div>
                         <div className = {styles.companyText}>
                             <h4 className={styles.cardSubtitle}>Mind Pump Media, June 2017 - August 2019</h4>
@@ -121,8 +126,11 @@ export default function ParallaxCard( {list}  ) {
                 <h3 className={styles.cardTitle}>Education</h3>
                 <div className={styles.companyContainer}>
                         <div className={styles.company}>
-                            <Image src="/usc.jpeg" alt="usc" className = {styles.companyImage} width={48} height={48} />
+                            <a href = "https://usc.edu">
+                              <Image src="/usc.jpeg" alt="usc" className = {styles.companyImage} width={48} height={48} />
+                            </a>
                         </div>
+
                         <div className = {styles.companyText}>
                             <h4 className={styles.cardSubtitle}>USC, August 2019 - December 2022</h4>
                             <p className = {styles.cardText} >Computer Science & Business</p>
@@ -139,10 +147,10 @@ export default function ParallaxCard( {list}  ) {
                         <div key={item.id} className={styles.companyContainer}>
                         <div className={styles.company}>
                             <Image unoptimized src={ "https://s2.googleusercontent.com/s2/favicons?domain_url=" + item.properties.URL.url +"&sz=64" } height={20} width={20} alt="url favicon" />
-                          
-                            <a className = {styles.title} href = {item.properties.URL.url}>
-                                {item.properties.Name.title[0]?.plain_text || ""}
+                            <a className={styles.title} target="_blank" href={`https://${item.properties.URL.url}`}>
+                              {item.properties.Name.title[0]?.plain_text || ""}
                             </a>
+
                             
                         </div>
                         </div>
